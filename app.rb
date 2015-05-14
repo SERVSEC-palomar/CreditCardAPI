e023ce140f23a7b03633dad7d8c79c689518df6d
Mark Noble <noble.m.a@gmail.com>
2015-05-14 14:50:19 +0800

    All done, well testing left..

--- app.rb
+++ app.rb
@@ -111,11 +111,13 @@ class CreditCardAPI < Sinatra::Base
   end
 
   get '/api/v1/credit_card/everything' do
+    haml :everything, locals: {
     CreditCard.all.map do |card|
       {
         number: card.number, expiration_date: card.expiration_date,
         owner: card.owner, credit_network: card.credit_network
       }.to_json
+    }
     end
   end
 end
